36235054aa193cde74495b7534d31103b3d6c930
John Rudell <johnrudell@Johns-MacBook-Pro.local>
2017-04-27 12:57:08 -0400

    media queries and footer

--- script.js
+++ script.js
@@ -6,13 +6,17 @@ $(document).ready(function(){
 		$(this).hover(function (e) {
 			$("#portDiv").css("background", "rgba(0, 0, 0, 0.6)");
 			$('#text' + num[i]).css("display", "inline");
-			// $('#name' + num[i]).css("font-size", "30px");
+			// $('#portDiv').fadeIn(1000);
+			$('#button' + num[i]).attr("src", "svgs/on-buttons/button" + num[i] +".svg");
+			// $('#button' + num[i]).animate({height: '180px', opacity: '1'}, "slow");
 			// $('#name' + num[i]).css("background", "rgba(119, 187, 255, 0.9)");
 			// $(".skill-name a h3").css("background", "rgba(119, 187, 255, 0.5)")
 			$(".main-column").css("opacity", "0.5");
 		}, function () {
 			$("#portDiv").css("background", "transparent");
 			$('#text' + num[i]).css("display", "none");
+			$('#button' + num[i]).attr("src", "svgs/off-buttons/button" + num[i] +".svg");
+			// $('#button' + num[i]).animate({height: '160px', opacity: '0.9'}, "slow");
 			// $('#name' + num[i]).css("font-size", "24px");
 			// $('#name' + num[i]).css("background", "rgba(119, 187, 255, 0.5)");
 			// $(".skill-name a h3").css("background", "rgba(119, 187, 255, 0.9)")
@@ -20,10 +24,15 @@ $(document).ready(function(){
 		})
 	});
 
-	$('.main-image').hover(function() {
+	$('.main-image, .about-content, .list-inline').hover(function() {
 		$('.about-content').css("display", "inline");
+		// $('.about-content').fadeIn(1000);
+		// $('.about-content').animate({opacity: '0.9'}, 1000);
 	}, function () {
+		// $('.about-content').fadeOut(1000);
 		$('.about-content').css("display", "none");
+		// $('.about-content').animate({opacity: '0.2'}, "slow");
 	})
+
 
 });
