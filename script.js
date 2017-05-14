349edbc0d5cf781b5feb7091101cdd3d29342299
John Rudell <johnrudell@Johns-MacBook-Pro.local>
2017-05-14 00:17:40 -0400

    icon summary switches

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 $(document).ready(function(){
 
+
+	// Show sample preview
 	var num = ['1', '2', '3', '4', '5']
 
-	$('.skill-name').each(function(i, value) {
+	$('.sample-name').each(function(i, value) {
 		$(this).hover(function (e) {
 			$("#portDiv").css("background", "rgba(0, 0, 0, 0.6)");
 			$('#text' + num[i]).css("display", "inline");
@@ -16,15 +18,36 @@ $(document).ready(function(){
 		})
 	});
 
+	// Show default summary
 	$('.main-image, .about-content, .list-inline').hover(function() {
 		$('.about-content').css("display", "inline");
 		$('.edu, .exp').css("margin", "0 0");
 	}, function () {
 		$('.about-content').css("display", "none");
 		$('.edu, .exp').css("margin", "0 auto");
-	})
+	});
 
+	// Show only main-summary
+	$('.main-image, .about-content').hover(function() {
+		$('.main-summary').css("display", "inline");
+	}, function () {
+		$('.main-summary').css("display", "none");
+	});
 
+	var numb = ['1', '2', '3', '4', '5','6']
+
+	// Show different icon summaries
+	$('.icon-name').each(function(i, value) {
+		$(this).hover(function (e) {
+			$('.icoText' + numb[i]).css("display", "inline");
+			$('.main-summary').css("display", "none");
+		}, function () {
+			$('.icoText' + numb[i]).css("display", "none");
+			$('.main-summary').css("display", "inline");
+		})
+	});
+
+	// Show iframe
 	$('.showIframe').click("slow", function() {
 		$('.siteSample').toggle("slow", function() {});
 		$('.modal-body').toggle("slow", function() {});
