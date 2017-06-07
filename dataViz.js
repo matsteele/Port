9f8bae41ee88c20183396e64160a53e2cff49c21
John Rudell <johnrudell@Johns-MacBook-Pro.local>
2017-06-07 16:47:51 -0400

    Fixed d3 circles not toggling, added resume label

--- dataViz.js
+++ dataViz.js
@@ -88,7 +88,7 @@ d3.json("http://matsteele.com/CityData2.json", function(error, data) {
  	     	.attr("cy", function (d) {
  	     		return d.properties.cy2;
  	     	})
- 	     	.attr("r", 2.5)
+ 	     	.attr("r", 2)
  	     	// .attr("z-indx", "5")
  	     	.attr("fill", "rgba(254,193,13, 0.75)")
  	     	.on("mouseover", function() {
