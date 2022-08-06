125767bd97e236398783c3b184511ad5ca1137f1
matsteele <matsteele@gmail.com>
2022-08-06 14:14:18 -0400

    shift to update

--- src/components/controller/contact/index.js
+++ src/components/controller/contact/index.js
@@ -8,47 +8,55 @@ import Cell from "./cell.svg";
 
 
 const contactMethods = {
-  email: {
-    image: Email,
-    descr: `Email is always the best way to reach me. I try to respond within two days to brief emails. `,
-    link: "mailto:abc@example.com?subject = Feedback&body = Message",
-    sub_title: "matsteele@gmail.com"
+  'where I have been': {
+    image: Email, // profile image
+    descr: `  `, 
+    // I celebrate my successes sure, but my failtures were more defining and probably more interesting. 
+    // I fear I failed the network of idealistic motivated and highly capable individuals I built with CoFED and KCFC
+    // as salaries and revenues weren't significant enough to sustain in the anti-captilist co-op world they existed in
+    // I failed at helping move Indonesia toward a more liberal urban future. 
+    // The more the World Bank pushed for more sustainable development, the stronger the country pushed back against western influence. 
+    // I failed at building a start up. I learned more in these two years than in either of my graduate degrees doing so. 
+    // 
+    link: 'mailto:abc@example.com?subject = Feedback&body = Message', // / linked In link
+    sub_title: 'matsteele@gmail.com',
   },
-  cell: {
-    image: Cell,
-    descr: `I suggest you send a text, as voicemails are often backlogged. WhatsApp or Telegram would be even more effective`,
-    sub_title: "206.920.4676"
+  'where I am going': {
+    image: Github, // video circulating with a drone 
+    descr: `My github is reflective of my non-private dashboards`, 
+    // somewhere calm, full of thoughtful relationships, and ideally a view
+    // 
+    // I have failed a lot in life, and , living a lot of life, looking for substance / clear solutions grounded in pressing problems
+    // keep this short 
+    // passionate about the power of private networks,  (make everything you say paradoxical)
+    code: 'https://github.com/matsteele/multifamily_analysis',// make link work twice link 1, and link 2
+    link: 'https://www.linkedin.com/in/matsteele/', // github link
+    sub_title: 'genus development partners',
   },
-  github: {
-    image: Github,
-    descr: `My github is reflective of my non-private dashboards`,
-    link: "https://www.linkedin.com/in/matsteele/",
-    sub_title: "genus development partners"
-  },
-  facebook: {
+  contact: {
     image: Facebook,
     descr: `Facebook is mostly for close friends`,
-    link: "https://www.facebook.com/Matthew.Stephan.Steele",
-    sub_title: "matthew.stephan.steele"
+    link: 'https://www.facebook.com/Matthew.Stephan.Steele',
+    sub_title: 'matthew.stephan.steele',
   },
   twitter: {
     image: Twitter,
     descr: `follow my ramblings`,
-    link: "https://twitter.com/urbanagrapher",
-    sub_title: "urbanagrapher"
+    link: 'https://twitter.com/urbanagrapher',
+    sub_title: 'urbanagrapher',
   },
   instagram: {
     image: Instagram,
     descr: `see a manufactured glimpse into my life`,
-    link: "https://www.instagram.com/matsteele/",
-    sub_title: "matsteele"
+    link: 'https://www.instagram.com/matsteele/',
+    sub_title: 'matsteele',
   },
   linkedin: {
     image: LinkedIn,
     descr: `Reach out and connect if you are a developer or looking for one.`,
-    link: "https://www.linkedin.com/in/matsteele/",
-    sub_title: "matsteele"
-  }
+    link: 'https://www.linkedin.com/in/matsteele/',
+    sub_title: 'matsteele',
+  },
 };
 
 export default contactMethods;
---
+++ todo.md
@@ -0,0 +1,20 @@
+
+
+TASKS 
+
+* fix github Link
+* change ways to connect to who am I 
+    Add resume  
+
+* escaping at contact sheet bugs out
+
+
+* solve secure connection issues
+* make options visible without having to scroll - like a legend to what's being scrolled
+
+Build a new one for projects
+
+* progeny - a platform for tokenizing real estate equity 
+* KCFC - thriving food co-op
+* CoFED - Food co-op incubator - in my idealistic youth I banded together with my food and health focused friends to create infrastructure for food co-ops at universities. 
+
